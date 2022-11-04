279a7548b0572c200316db8ad2156c5e236a4dcb
Isaad Khan <IKhan@lcs.on.ca>
2022-11-04 08:57:51 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Isaad Khan on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
